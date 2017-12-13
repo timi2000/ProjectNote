613f89764c486c99eacef7360220d4fbd8ce935a
Tim Widmer <timwidmer@der-bessere-Timothy-MacBook-Pro.local>
2017-12-13 21:30:04 +0100

    initial project

---
+++ projectnote/src/main/java/com/tim/projectnote/ProjectnoteApplication.java
@@ -0,0 +1,12 @@
+package com.tim.projectnote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectnoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectnoteApplication.class, args);
+	}
+}
